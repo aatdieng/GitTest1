0c8785b5bc3c4508f885a5f7cd8741d3c04270a5
Alassane Alboury Tanor Dieng <aatdieng@gmail.com>
2018-06-08 10:25:25 +0000

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //kirasoulay Git rek
             Console.WriteLine("ceci est un test de tout ca ");
             Console.ReadKey();
         }
